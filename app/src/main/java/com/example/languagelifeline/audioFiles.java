5195074d0961c98fea652d10a00c4c6f12dd4e06
Nick Souligne <44684548+Babyshaqdos@users.noreply.github.com>
2021-11-26 17:00:54 -0600

    EM additions and adapter changes

--- app/src/main/java/com/example/languagelifeline/audioFiles.java
+++ app/src/main/java/com/example/languagelifeline/audioFiles.java
@@ -1,32 +1,57 @@
 package com.example.languagelifeline;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class audioFiles {
-    private List<String> engAudioFiles;
-    private List<String> spanishAudioFiles;
-    private List<String> frenchAudioFiles;
+    static Map<String, Integer> engPatientAudio = new HashMap<String, Integer>();
+    static Map<String, Integer> engProviderAudio = new HashMap<String, Integer>();
+    static Map<String, Integer> spanPatientAudio = new HashMap<String, Integer>();
+    static Map<String, Integer> spanProviderAudio = new HashMap<String, Integer>();
+    static Map<String, Integer> frenchPatientAudio = new HashMap<String, Integer>();
+    static Map<String, Integer> frenchProviderAudio = new HashMap<String, Integer>();
 
 
     public audioFiles(){
-        engAudioFiles = new ArrayList<>();
-       // englishFiles = {R.raw.i_need_to_use_the_toilet, R.raw.i_would_like_some_food, R.raw.i_would_like_some_water, R.raw.what_is_this, R.raw.my_pain_is_better, R.raw.my_pain_is_worse, R.raw.my_pain_is_the_same, R.raw.my_stomach_hurts, R.raw.i_am_dizzy, R.raw.i_do_not_feel_good, R.raw.i_feel_weak, R.raw.i_am_having_trouble_breathing, R.raw.i_feel_nauseous, R.raw.can_you_check_my_vitals, R.raw.can_you_charge_my_phone, R.raw.i_need_to_make_a_phone_call, R.raw.can_you_close_my_blinds, R.raw.can_you_open_my_blinds, R.raw.i_cannot_sleep, R.raw.can_i_have_a_pillow, R.raw.can_i_have_a_blanket, R.raw.can_i_change_my_position, R.raw.i_feel_uncomfortable, R.raw.i_would_like_that_later}
-        engAudioFiles.add("R.raw.i_need_to_use_the_toilet");
+       engPatientAudio.put("R.raw.i_need_to_use_the_toilet", R.raw.i_need_to_use_the_toilet);
+       engPatientAudio.put("R.raw.i_would_like_some_food", R.raw.i_would_like_some_food);
+       engPatientAudio.put("R.raw.i_would_like_some_water", R.raw.i_would_like_some_water);
+       engPatientAudio.put("R.raw.what_is_this", R.raw.what_is_this);
+       engPatientAudio.put("R.raw.my_pain_is_better", R.raw.my_pain_is_better);
+       engPatientAudio.put("R.raw.my_pain_is_worse", R.raw.my_pain_is_worse);
+       engPatientAudio.put("R.raw.my_pain_is_the_same", R.raw.my_pain_is_the_same);
+       engPatientAudio.put("R.raw.my_stomach_hurts", R.raw.my_stomach_hurts);
+       engPatientAudio.put("R.raw.i_am_dizzy", R.raw.i_am_dizzy);
+       engPatientAudio.put("R.raw.i_do_not_feel_good", R.raw.i_do_not_feel_good);
+       engPatientAudio.put("R.raw.i_feel_weak", R.raw.i_feel_weak);
+       engPatientAudio.put("R.raw.i_am_having_trouble_breathing", R.raw.i_am_having_trouble_breathing);
+       engPatientAudio.put("R.raw.i_feel_nauseous", R.raw.i_feel_nauseous);
+       engPatientAudio.put("R.raw.i_think_i_have_a_fever_can_you_check_my_temperature", R.raw.i_think_i_have_a_fever_can_you_check_my_temperature);
+       engPatientAudio.put("R.raw.can_you_check_my_vitals", R.raw.can_you_check_my_vitals);
+       engPatientAudio.put("R.raw.can_you_charge_my_phone", R.raw.can_you_charge_my_phone);
+       engPatientAudio.put("R.raw.i_need_to_make_a_phone_call", R.raw.i_need_to_make_a_phone_call);
+       engPatientAudio.put("R.raw.can_you_close_my_blinds", R.raw.can_you_close_my_blinds);
+       engPatientAudio.put("R.raw.can_you_open_my_blinds", R.raw.can_you_open_my_blinds);
+       engPatientAudio.put("R.raw.i_cannot_sleep", R.raw.i_cannot_sleep);
+       engPatientAudio.put("R.raw.i_would_like_some_medication_to_help_me_sleep", R.raw.i_would_like_some_medication_to_help_me_sleep);
+       engPatientAudio.put("R.raw.can_i_have_a_pillow", R.raw.can_i_have_a_pillow);
+       engPatientAudio.put("R.raw.can_i_have_a_blanket", R.raw.can_i_have_a_blanket);
+       engPatientAudio.put("R.raw.can_i_move_to_the_chair", R.raw.can_i_move_to_the_chair);
+       engPatientAudio.put("R.raw.can_i_raise_my_head", R.raw.can_i_raise_my_head);
+       engPatientAudio.put("R.raw.can_i_lower_my_head", R.raw.can_i_lower_my_head);
+       engPatientAudio.put("R.raw.can_i_change_my_position", R.raw.can_i_change_my_position);
+       engPatientAudio.put("R.raw.i_feel_uncomfortable", R.raw.i_feel_uncomfortable);
+       engPatientAudio.put("R.raw.i_would_like_to_do_that_myself", R.raw.i_would_like_to_do_that_myself);
+       engPatientAudio.put("R.raw.i_would_like_that_later", R.raw.i_would_like_that_later);
+       engPatientAudio.put("R.raw.may_i_see_the_nurse", R.raw.may_i_see_the_nurse);
+
 
     }
 
 
-    public List<String> getEngAudioFiles(){
-      return engAudioFiles;
-    };
 
-    public List<String> getSpanishAudioFiles(){
-        return spanishAudioFiles;
-    }
-    public List<String> getFrenchAudioFiles(){
-        return frenchAudioFiles;
-    }
 
 
 }
